1a6232851006246f14fac92e11e6661d8e818121
aaxxa <aaxxa@dev.aaxxa>
2020-01-16 16:57:12 +0200

    first commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+    	return view('index');
+    }
+
+    public function contact(){
+    	return view('contact');
+    }
+
+    public function cart(Request $request){
+    	return "hfvrf,.lv";
+    }
+}
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html>
+
+<!-- Mirrored from c.mta.sa/contact_us by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 15 Jan 2020 13:03:54 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta name="HandheldFriendly" content="true">
+<title>
+                    تسجيل طلب التحاضير
+            </title>
+<link rel="icon" type="image/png" href="{{ asset('images/fav-icon.png') }}">
+<link media="all" type="text/css" rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+<link media="all" type="text/css" rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+<link media="all" type="text/css" rel="stylesheet" href="{{asset('css/jquery.mCustomScrollbar.css')}}">
+<link media="all" type="text/css" rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+<body>
+
+<nav class="navbar">
+<div class="container">
+<div class="row">
+<div class="col-xs-12 col-sm-8 text-align-right">
+<a href="{{route('pages.index')}}" class="navbar-brand"><img src="{{asset('images/logo.png')}}" alt="LOGO"></a>
+</div>
+</div>
+<div class="row">
+<div class="col-xs-12 col-sm-3 pull-left text-align-left">
+<div class="social">
+<a href="https://www.facebook.com/www.mta.sa/?fref=ts" target="_blank" class="fb"><i class="fa fa-facebook"></i></a>
+<a href="https://twitter.com/thadeer5" tabindex="_blank" class="tw"><i class="fa fa-twitter"></i></a>
+<a href="https://www.mta.sa/" target="_blank" class="gp"><i class="fa fa-google-plus"></i></a>
+<a href="https://www.instagram.com/thader__/" target="_blank" class="inst"><i class="fa fa-instagram"></i></a>
+</div>
+</div>
+<div class="col-xs-12 col-sm-3 text-align-right">
+<p class="whats"><a href="tel:0551092444"><i class="fa fa-whatsapp right-fa"></i> 0551092444</a> </p>
+</div>
+<div class="col-xs-12 col-sm-6">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
+<span class="fa fa-bars"></span>
+</button>
+</div>
+<div class="collapse navbar-collapse" id="navbar-collapse-1">
+<ul class="nav navbar-nav text-center">
+<li class="  "><a href="{{route('pages.index')}}">الرئيسية</a></li>
+<li class="  active  "><a href="{{route('pages.contact')}}">اتصل بنا</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</nav>
+<div class="div-padding white-bg">
+<div class="container">
+<div class="row">
+<div class="col-xs-12 col-sm-10 col-sm-pull-1 col-ms-8 col-md-pull-2 col-lg-6 col-lg-pull-3">
+</div>
+<div class="col-ms-12 text-center">
+<h1 class="single_title" style="text-align: right;position: relative;left: 50px;">
+<i class="fa fa-file-o"></i>
+اتصل بنا
+</h1>
+<section class="block_single">
+<div class="container">
+<div class="post_block">
+<div class="post_text">
+<p style="text-align: center;"><span style="font-size: 14pt;"><strong><span style="color: #800000;">للتواصل على الهاتف أو الواتس آب</span></strong></span></p>
+<p style="text-align: center;"><span style="font-size: 14pt;"><strong>فقط اتصل الآن</strong></span></p>
+<div class="_5wd4 _1nc7 _5ysy">
+<div class="_h8t">
+<div class="_5wd9 direction_ltr">
+<div class="_5wde _n4o">
+<div class="_5w1r _3_om _5wdf">
+<div class="_4gx_">
+<div class="_d97" style="text-align: center;"><span style="font-size: 18pt;"><strong><span class="_5yl5" style="color: #000080;">0555107025 </span></strong></span></div>
+<div class="_d97" style="text-align: center;"><span style="color: #800000; font-size: 18pt;"><strong><span class="_5yl5">0557977722 </span></strong></span></div>
+<div class="_d97" style="text-align: center;"><span style="font-size: 18pt;"><strong><span class="_5yl5" style="color: #000080;">0505107025</span></strong></span></div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<p>&nbsp;</p>
+<div class="oewak5cdf54487ad29"><h3 style="text-align: center;"><span style="font-size: 14pt;"><strong>للطلب من المملكة يمكنكم التواصل مع الأرقام الجديدة والدائمة لنا علي الواتس</strong></span></h3>
+<p style="text-align: center;"><span style="font-size: 24pt;"><a href="https://api.whatsapp.com/send?phone=966555107025" target="_blanck">966555107025</a></span></p>
+<p style="text-align: center;"><span style="font-size: 24pt;"><a href="https://api.whatsapp.com/send?phone=966557977722" target="_blanck">966557977722</a></span></p>
+<h4 style="text-align: center;"><span style="font-size: 14pt;"><img class="alignnone size-medium aligncenter" width="500" height="214"></span></h4></div><style type="text/css">@media  screen and (min-width:1201px){.oewak5cdf54487ad29{display:block}}@media  screen and (min-width:993px) and (max-width:1200px){.oewak5cdf54487ad29{display:block}}@media  screen and (min-width:769px) and (max-width:992px){.oewak5cdf54487ad29{display:block}}@media  screen and (min-width:768px) and (max-width:768px){.oewak5cdf54487ad29{display:block}}@media  screen and (max-width:767px){.oewak5cdf54487ad29{display:block}}</style>
+</div>
+</div>
+</div>
+</section>
+</div>
+</div>
+</div>
+</div>
+<div id="content_new"></div>
+
+<footer class="div-small-padding">
+<div class="container">
+<div class="row">
+<div class="col-xs-12 col-sm-7 text-align-right">
+<p> جميع الحقوق محفوظة لمؤسسة التحاضير الحديثة 2019 </p>
+</div>
+<div class="col-xs-12 col-sm-5 text-align-left">
+<p>تصميم <a href="#">مؤسسة أوامر الشبكة</a></p>
+</div>
+</div>
+</div>
+</footer>
+
+<div class="pill-holder">
+<div class="btn pill-btn">الـــفـاتـــورة</div>
+<div class="pill-table mCustomScrollbar text-center">
+<table class="table">
+<thead>
+<tr class="green-bg">
+<td colspan="2">المادة</td>
+<td colspan="2">السعر</td>
+</tr>
+<tr class="blue-bg">
+<td>اسم المادة</td>
+<td>المرحلة</td>
+<td>السعر</td>
+<td><i class="fa fa-trash"></i></td>
+</tr>
+</thead>
+<tbody id="bill-tbody">
+<tr id="no_matrial_found">
+<td colspan="4">
+لاتوجد مواد مختاره حتى الان
+</td>
+</tr>
+</tbody>
+<tfoot>
+<tr class="green-bg">
+<td colspan="2">الاجمالى</td>
+<td colspan="2"> <span id="cart_total"> 0 </span> ريال</td>
+</tr>
+</tfoot>
+</table>
+</div>
+</div>
+
+<div class="modal fade" id="errorModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal-dialog modal-sm" role="document">
+<div class="modal-content text-center">
+<div class="modal-header text-left">
+<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+</div>
+<div class="modal-body">
+<p>برجاء اختر نوع الطلب اولا</p>
+</div>
+</div>
+</div>
+</div>
+
+<div class="pre-load" id="loader"> 
+<img src="{{asset('images/loading.gif')}}" alt="Loading...">
+</div>
+<script src="{{asset('js/jquery-1.11.1.min.js')}}"></script>
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+<script src="{{asset('js/jquery.mCustomScrollbar.concat.min.js')}}"></script>
+<script src="{{asset('js/script.js')}}"></script>
+<script src="{{asset('js/dev.js')}}"></script>
+<script src="{{asset('js/www.google.com/recaptcha/api.js')}}" async defer></script>
+<script src="{{asset('js/bootstrapious.com/tutorial/recaptcha/validator.js')}}" async defer></script>
+<script src="{{asset('js/unpkg.com/jspdf%401.5.3/dist/jspdf.min.js')}}"></script>
+<script src="{{asset('js/www.google.com/recaptcha/api.js')}}"></script>
+
+
+<script>
+    function verifyRecaptchaCallback(){
+        $('#rePatcha').removeAttr('disabled');
+    }
+
+    $(document).ready(function () {
+        var url = $('#url').data('url');
+        $('.set_url').attr('href', url);
+    });
+
+    $('.price_list').hover(function () {
+        if($('#select-delivery').val() == 5 || $('#select-delivery').val() == 6){
+            // alert('ops');
+            setTimeout(function () {$('.price_list_show').css("display", "none")}, 10);
+        }else {
+            setTimeout(function () {$('.thader_price').css("display", "none")}, 10);
+        }
+    });
+
+
+    $('#select-delivery').change(function () {
+        if($(this).val() == 5 || $(this).val() == 6) {
+            $('.atfal').fadeOut();
+            $('.kberat').fadeOut();
+            $('.fekriya').fadeOut();
+            $('.disapper').fadeOut();
+            //   $('.classy').fadeOut();
+            // setTimeout(function () {$('.price_list_show').css("display", "none")}, 10);
+        }else if( $(this).val() == 1){
+            $('.disapper').fadeOut();
+        }else {
+            $('.atfal').fadeIn();
+            $('.kberat').fadeIn();
+            $('.fekriya').fadeIn();
+            $('.disapper').fadeIn();
+            $('.classy').fadeIn();
+        }
+    });
+
+    $(document).ready(function () {
+        if( navigator.userAgent.match(/Android/i)
+            || navigator.userAgent.match(/webOS/i)
+            || navigator.userAgent.match(/iPhone/i)
+            || navigator.userAgent.match(/iPad/i)
+            || navigator.userAgent.match(/iPod/i)
+            || navigator.userAgent.match(/BlackBerry/i)
+            || navigator.userAgent.match(/Windows Phone/i)
+        ){
+            $('.check-mobile').css('display', 'none')
+        }
+        else {
+            return false;
+        }
+    })
+</script>
+</body>
+
+<!-- Mirrored from c.mta.sa/contact_us by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 15 Jan 2020 13:03:54 GMT -->
+</html>
